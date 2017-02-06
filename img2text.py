de8e65bbb1258d03c8309e19fe3893d9727d5091
Mihai Marica <mihai.marica82@yahoo.com>
2017-02-06 23:16:16 +0200

    finished implementing conversion algorithm

--- img2text.py
+++ img2text.py
@@ -1,14 +1,29 @@
 from PIL import Image
 import sys
 
+def pixel_to_char(rgb):
+    # list of available characters, sorted by luminosity: darker to lighter
+    chars = ("#", "?", "%", "$", "Q", "+", ",", "j", "*", "~", "`", ".", " ")
+
+    r, g, b = rgb
+    luminance = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+
+    return chars[int((len(chars)) * luminance / 255) - 1]
+
+# maximum image size allowed
+max_width = 2000
+max_height = 2000
+
+# how many times the image should be scaled down before transforming each pixel into a text character
 downscale = 5
-max_width = 1000
-max_height = 1000
+
+# multiplier for height when resizing, to compensate for the console characters shape, which are taller than wider
+height_multiplier = 0.6
+
 
 if len(sys.argv) != 2:
     print "Please specify an image file/url"
     sys.exit(1)
-
 
 filename = sys.argv[1]
 
@@ -25,11 +40,16 @@ if width > max_width or height > max_height:
     print "[ERROR] Image larger than allowed size of %ix%i px" % (max_width, max_height)
     sys.exit(1)
 
-size = (width / downscale, height / downscale)
-print "Downscale factor %i, new size: %ix%i px" % (downscale, size[0], size[1])
+size = (width / downscale, int(height * height_multiplier / downscale))
+print "Downscale factor %i, height multiplier %0.1f, new size: %ix%i px" % (downscale, height_multiplier, size[0], size[1])
 
 
-resized_img = img.resize(size, Image.ANTIALIAS)
-new_img = Image.new(mode='RGBA', size=size, color=(255, 255, 255, 0));
-new_img.paste(resized_img, (0, 0));
-new_img.save('test.png')
+resized_img = img.resize(size, Image.ANTIALIAS).convert('RGB');
+
+output = ""
+for y in range(size[1]):
+    for x in range(size[0]):
+        output += pixel_to_char(resized_img.getpixel((x, y)))
+    output += "\n"
+
+print output
